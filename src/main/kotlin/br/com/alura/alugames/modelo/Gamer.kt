98b7d5906f0db65946b3dd705ab7c6c26039c15d
jessicaraissapessoa <jessicaraissapessoa@gmail.com>
2024-03-24 17:33:26 -0300

    Aula: Validando informações

--- src/main/kotlin/br/com/alura/alugames/modelo/Gamer.kt
+++ src/main/kotlin/br/com/alura/alugames/modelo/Gamer.kt
@@ -22,6 +22,13 @@ data class Gamer(var nome : String, var email : String) {
         criarIdInterno()
     }
 
+    init {
+        if (nome.isNullOrBlank()) {
+            throw IllegalArgumentException("Nome está em branco")
+        }
+        this.email = validarEmail() //Antes de executar criação do objeto, vai executar esse init com validação do email
+    }
+
     override fun toString(): String {
         return "Gamer(nome='$nome', email='$email', dataNascimento=$dataNascimento, usuario=$usuario, idInterno=$idInterno)"
     }
@@ -31,6 +38,16 @@ data class Gamer(var nome : String, var email : String) {
         val tag = String.format("%04d", numero)
 
         idInterno = "$usuario#$tag"
+    }
+
+    fun validarEmail() : String {
+        val regex = Regex("^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$")
+
+        if (regex.matches(email)) {
+            return email
+        } else {
+            throw IllegalArgumentException("Email inválido")
+        }
     }
 
 }
--- src/main/kotlin/br/com/alura/alugames/principal/TesteGamer.kt
+++ src/main/kotlin/br/com/alura/alugames/principal/TesteGamer.kt
@@ -1,7 +1,7 @@
 import br.com.alura.alugames.modelo.Gamer
 
 fun main() {
-    val gamer1 = Gamer("Jacque", "jacque@email.com")
+    val gamer1 = Gamer("   ", "jacque@email.com")
     println(gamer1)
 
     val gamer2 = Gamer("Jeni", "jeni@email.com", "19/19/1992", "jeniblo")
