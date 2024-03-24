23e8622058a51657ec0856f61dec19ce66d451ec
jessicaraissapessoa <jessicaraissapessoa@gmail.com>
2024-03-24 19:10:54 -0300

    Aula: Manipulando a lista

--- src/main/kotlin/br/com/alura/alugames/principal/Main.kt
+++ src/main/kotlin/br/com/alura/alugames/principal/Main.kt
@@ -59,6 +59,32 @@ fun main() {
     println("Jogos buscados:")
     println(gamer.jogosBuscados)
 
+    println("\nJogos ordenados por título:")
+    gamer.jogosBuscados.sortBy {
+        it?.titulo
+    }
+
+    gamer.jogosBuscados.forEach {
+        println("Título: " + it?.titulo)
+    }
+
+    val jogosFiltrados = gamer.jogosBuscados.filter {
+        it?.titulo?.contains("batman", true) ?: false
+    }
+    println("\nJogos filtrados: ")
+    println(jogosFiltrados)
+
+    println("Deseja excluir algum jogo da lista original? S/N")
+    val opcao = leitura.nextLine()
+    if (opcao.equals("s", true)) {
+        println(gamer.jogosBuscados)
+        println("\nInforme a posição do jogo que deseja excluir:")
+        val posicao = leitura.nextInt()
+        gamer.jogosBuscados.removeAt(posicao)
+    }
+    println("\nLista atualizada:")
+    println(gamer.jogosBuscados)
+
     println("Busca finalizada com sucesso.")
 
 }
